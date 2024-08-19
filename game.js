1adb4d83f2917bb09ca272735f56d41a6e9d2c34
Trueman Phamdo <trueman@phamdo.com>
2024-08-19 09:58:04 -0400

    Add game ui

--- game.js
+++ game.js
@@ -1,9 +1,40 @@
 const choices = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
+const moves = document.querySelector("#moves");
+const score = document.querySelector("#score");
+const buttons =  document.querySelectorAll("#buttons button");
+const boxScore = document.querySelector(".box-score");
+const restart = document.querySelector("#restart");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (humanScore === 5 || computerScore === 5) return;
+        const hc = button.id;
+        const cc = getComputerChoice();
+
+        [hS, cS] = playRound(cc, hc);
+        humanScore += hS, computerScore += cS;
+        moves.textContent = `You: ${hc}. Computer: ${cc}`
+        score.textContent = `You - Computer: ${humanScore} - ${computerScore}`
+
+        if (humanScore === 5 || computerScore === 5) {
+            const result = document.createElement('p')
+            result.textContent = humanScore === 5? "You win!" : "Computer wins!";
+            boxScore.appendChild(result);
+        }
+    })
+});
+
+restart.addEventListener("click", () => {
+    humanScore = 0, computerScore = 0;
+    moves.textContent = null;
+    score.textContent = `You - Computer: ${humanScore} - ${computerScore}`
+    if (boxScore.childElementCount === 3) boxScore.removeChild(boxScore.lastChild);
+});
 
 function playGame() {
     const NUM_ROUNDS = 5;
-    let humanScore = 0;
-    let computerScore = 0;
 
     for (let i = 0; i<NUM_ROUNDS; ++i) {
         const hc = getHumanChoice();
